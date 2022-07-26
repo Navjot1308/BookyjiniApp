761046762d6eb546d7a2e902fca2718427bfea4a
Navjot Singh <navjot.s3101@gmail.com>
2022-07-26 11:43:08 +0530

    Bookyjini App

---
+++ app/src/main/java/com/gravestone/bookyjini/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.gravestone.bookyjini
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
